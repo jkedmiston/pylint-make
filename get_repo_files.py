69b536f24a35234c4fc7772bc7ed6bc878f64159
John Edmiston <johnkedmiston@gmail.com>
2021-02-03 18:37:41 -0800

    make get_repo_files pass pylint

--- get_repo_files.py
+++ get_repo_files.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
-# https://stackoverflow.com/questions/8533202/list-files-in-local-git-repo
+"""
+Based on the git command here, then filters for python files.
+https://stackoverflow.com/questions/8533202/list-files-in-local-git-repo
+"""
 import sys
 import subprocess
 
-proc = subprocess.Popen(["git ls-tree --full-tree -r --name-only HEAD"],
-                        shell=True,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
-stdout, stderr = proc.communicate()
-files_in_repo = stdout.decode('utf-8').strip().split('\n')
-final_files = list(filter(lambda x: x[-3:] == '.py', files_in_repo))
-sys.stdout.write(' '.join(final_files) + '\n')
+
+def get_git_python_files():
+    """
+    Get a list of all files in repository control, then filter the
+    python files, and write to stdout, similar to an ls command.
+    """
+    proc = subprocess.Popen(["git ls-tree --full-tree -r --name-only HEAD"],
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout, _ = proc.communicate()
+    files_in_repo = stdout.decode('utf-8').strip().split('\n')
+    final_files = list(filter(lambda x: x[-3:] == '.py', files_in_repo))
+    sys.stdout.write(' '.join(final_files) + '\n')
+
+
+if __name__ == "__main__":
+    get_git_python_files()
